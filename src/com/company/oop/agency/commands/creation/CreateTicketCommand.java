9b4f806652e6d61b5170e988ccfeb1e4d7f311ee
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2024-10-29 00:07:05 +0200

    implementing "CreateTicketCommand"

--- src/com/company/oop/agency/commands/creation/CreateTicketCommand.java
+++ src/com/company/oop/agency/commands/creation/CreateTicketCommand.java
@@ -1,7 +1,48 @@
 package com.company.oop.agency.commands.creation;
 
-public class CreateTicketCommand {
+import com.company.oop.agency.commands.contracts.Command;
+import com.company.oop.agency.core.contracts.AgencyRepository;
+import com.company.oop.agency.models.TicketImpl;
+import com.company.oop.agency.models.contracts.Journey;
+import com.company.oop.agency.utils.ParsingHelpers;
+import com.company.oop.agency.utils.ValidationHelper;
+
+import java.util.List;
+
+public class CreateTicketCommand implements Command {
+
 
     public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 2;
+    private static final String TICKET_CREATED_MESSAGE = "Ticket with ID %d was created.";
+
+    private final AgencyRepository repository;
+    private int journeyId;
+    private double cost;
+
+
+    public CreateTicketCommand(AgencyRepository repository) {
+        this.repository = repository;
+    }
+
+
+    @Override
+    public String execute(List<String> parameters) {
+
+        ValidationHelper.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
+
+        parseParameters(parameters);
+
+        Journey journey = repository.findJourneyById(journeyId);
+        TicketImpl ticket = repository.createTicket(journey,cost);
+
+        return String.format(TICKET_CREATED_MESSAGE, ticket.getId());
+
+    }
+
+
+    private void parseParameters(List<String> parameters) {
+        this.journeyId = ParsingHelpers.tryParseInteger(parameters.get(0), "ticket id");
+        this.cost = ParsingHelpers.tryParseDouble(parameters.get(1), "cost");
+    }
 
 }
