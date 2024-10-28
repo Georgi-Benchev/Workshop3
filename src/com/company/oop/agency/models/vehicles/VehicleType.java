b7dbaa62ddb9213d8a7c21014cd6aebab3b11385
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2024-10-28 21:26:49 +0200

    implementing enum VehicleType

--- src/com/company/oop/agency/models/vehicles/VehicleType.java
+++ src/com/company/oop/agency/models/vehicles/VehicleType.java
@@ -1,5 +1,23 @@
 package com.company.oop.agency.models.vehicles;
 
 public enum VehicleType {
-    // TODO
+
+    LAND,
+    AIR,
+    SEA;
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case LAND:
+                return "Land";
+            case AIR:
+                return "Air";
+            case SEA:
+                return "Sea";
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+
 }
