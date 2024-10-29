ad1b348c7578d0682ff72d4d0815462397ed7371
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2024-10-29 15:33:24 +0200

    using generics for all "Printable" classes

--- src/com/company/oop/agency/utils/ListingHelpers.java
+++ src/com/company/oop/agency/utils/ListingHelpers.java
@@ -1,9 +1,12 @@
 package com.company.oop.agency.utils;
 
 import com.company.oop.agency.commands.CommandsConstants;
+import com.company.oop.agency.exceptions.ElementNotFoundException;
 import com.company.oop.agency.models.JourneyImpl;
 import com.company.oop.agency.models.TicketImpl;
+import com.company.oop.agency.models.contracts.Identifiable;
 import com.company.oop.agency.models.contracts.Journey;
+import com.company.oop.agency.models.contracts.Printable;
 import com.company.oop.agency.models.contracts.Ticket;
 import com.company.oop.agency.models.vehicles.contracts.Vehicle;
 
@@ -13,33 +16,25 @@ import java.util.List;
 public class ListingHelpers {
 
     public static String ticketsToString(List<TicketImpl> tickets) {
-        List<String> result = new ArrayList<>();
-        for (TicketImpl ticket : tickets) {
-            result.add(ticket.getAsString());
-        }
-
-        return String.join(CommandsConstants.JOIN_DELIMITER + System.lineSeparator(), result).trim();
+        return elementsToString(tickets);
     }
 
     public static String vehiclesToString(List<Vehicle> vehicles) {
-        List<String> result = new ArrayList<>();
-        for (Vehicle vehicle : vehicles) {
-            result.add(vehicle.getAsString());
-        }
-
-        return String.join(CommandsConstants.JOIN_DELIMITER + System.lineSeparator(), result).trim();
+        return elementsToString(vehicles);
     }
 
     public static String journeysToString(List<JourneyImpl> journeys) {
-        List<String> result = new ArrayList<>();
-        for (JourneyImpl journey : journeys) {
-            result.add(journey.getAsString());
-        }
-
-        return String.join(CommandsConstants.JOIN_DELIMITER + System.lineSeparator(), result).trim();
+        return elementsToString(journeys);
     }
 
-//    public static <T extends Printable> String elementsToString(List<T> elements) {
-//    }
+//    public static <T extends Printable> String elementsToString(List<T> elements)
+
+    public static <E extends Printable> String elementsToString(List<E> printableElements) {
+        List<String> result = new ArrayList<>();
+        for (E element : printableElements) {
+            result.add(element.getAsString());
+        }
+        return String.join(CommandsConstants.JOIN_DELIMITER + System.lineSeparator(), result).trim();
+    }
 
 }
